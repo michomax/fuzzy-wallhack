1b12816ccc27e51a509a293ddeed820cc3053028
michomax <maxime.michomax>
2014-02-12 16:01:03 +0100

    Modif Types

--- TP04/types.ml
+++ TP04/types.ml
@@ -1,17 +1,5 @@
-(*type term =
-    | True (* Constante Vrai *)
-    | False (* Constante Faux *)
-    | Cond of term * term * term (* Conditionnel *)
-    | Zero (* Zero *)
-    | Succ of term (* Successeur *)
-    | Pred of term (* Predecesseur *)
-    | Iszero of term (* Test Nul *)
-    | None
-;;*)
-
 (*Type Lambda-calcul*)
 type term = 
-  | Lambda
   | Var of string
-  | Abs of Lambda * Var * term
+  | Abs of Var * term
   | App of term * term
